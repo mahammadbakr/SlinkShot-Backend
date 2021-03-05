f93cd451eb3a35115cf24695376f0467b76ff51d
Muhammad M Bakr <mahammadbakr77@gmail.com>
2021-03-05 20:36:01 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secret': 'SeCrEtKeYfOrHaShInG',
-    'mongodbUri': 'mongodb+srv://slinkshot:slinkshot2202@slinkshot0.prag8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    'mongodbUri': 'mongodb+srv://slinkshot:<password>@slinkshot0.prag8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
